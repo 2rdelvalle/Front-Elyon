d86778364349ae360dfd799951028ebfc456d7c0
2rdelvalle <networking@elyonyireh.edu.co>
2024-12-29 18:12:17 -0500

    login succes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,24 +10,29 @@ export class LoginComponent {
   email: string = '';
   password: string = '';
   errorMessage: string = '';
+  loading: boolean = false;
 
   constructor(private router: Router) { }
 
   onSubmit() {
+    this.loading = true;
     if (this.email && this.password) {
       // Lógica de autenticación
       this.authenticate(this.email, this.password).then(success => {
         if (success) {
-          // Redireccionar a la página principal después del login exitoso
-          this.router.navigate(['/']);
+          // Redireccionar a la página de dashboard después del login exitoso
+          this.router.navigate(['/pages/dashboard']);
         } else {
           this.errorMessage = 'Email o contraseña incorrectos.';
         }
       }).catch(error => {
         this.errorMessage = 'Ocurrió un error. Por favor, intenta nuevamente.';
+      }).finally(() => {
+        this.loading = false;
       });
     } else {
       this.errorMessage = 'Por favor, ingresa tu email y contraseña.';
+      this.loading = false;
     }
   }
 

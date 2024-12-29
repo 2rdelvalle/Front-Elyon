3e6d680929f4cd69f306517b86bf99aff37a6d56
2rdelvalle <networking@elyonyireh.edu.co>
2024-12-29 14:15:23 -0500

    initial commit

---
+++ src/app/pages/pages-menu.ts
@@ -0,0 +1,247 @@
+import { NbMenuItem } from '@nebular/theme';
+
+export const MENU_ITEMS: NbMenuItem[] = [
+  {
+    title: 'Home',
+    icon: 'shopping-cart-outline',
+    link: '/pages/dashboard',
+    home: true,
+  },
+  {
+    title: 'Administrativo',
+    icon: 'home-outline',
+    link: '/pages/iot-dashboard',
+  },
+  {
+    title: 'FEATURES',
+    group: true,
+  },
+  {
+    title: 'Financiero',
+    icon: 'layout-outline',
+    children: [
+      {
+        title: 'Estado financiero',
+        link: '/pages/layout/estado financiero',
+      },
+      {
+        title: 'Liquidacion de conceptos',
+        link: '/pages/layout/Liquidacion de conceptos',
+      },
+      {
+        title: 'Historial de liquidacion',
+        link: '/pages/layout/historial de liquidacion',
+      },
+      {
+        title: 'Credito financiero',
+        link: '/pages/layout/credito financiero',
+      },
+      {
+        title: 'Liquidacion de matricula',
+        pathMatch: 'prefix',
+        link: '/pages/layout/liquidar matricula',
+      },
+    ],
+  },
+  {
+    title: 'Forms',
+    icon: 'edit-2-outline',
+    children: [
+      {
+        title: 'Form Inputs',
+        link: '/pages/forms/inputs',
+      },
+      {
+        title: 'Form Layouts',
+        link: '/pages/forms/layouts',
+      },
+      {
+        title: 'Buttons',
+        link: '/pages/forms/buttons',
+      },
+      {
+        title: 'Datepicker',
+        link: '/pages/forms/datepicker',
+      },
+    ],
+  },
+  {
+    title: 'UI Features',
+    icon: 'keypad-outline',
+    link: '/pages/ui-features',
+    children: [
+      {
+        title: 'Grid',
+        link: '/pages/ui-features/grid',
+      },
+      {
+        title: 'Icons',
+        link: '/pages/ui-features/icons',
+      },
+      {
+        title: 'Typography',
+        link: '/pages/ui-features/typography',
+      },
+      {
+        title: 'Animated Searches',
+        link: '/pages/ui-features/search-fields',
+      },
+    ],
+  },
+  {
+    title: 'Modal & Overlays',
+    icon: 'browser-outline',
+    children: [
+      {
+        title: 'Dialog',
+        link: '/pages/modal-overlays/dialog',
+      },
+      {
+        title: 'Window',
+        link: '/pages/modal-overlays/window',
+      },
+      {
+        title: 'Popover',
+        link: '/pages/modal-overlays/popover',
+      },
+      {
+        title: 'Toastr',
+        link: '/pages/modal-overlays/toastr',
+      },
+      {
+        title: 'Tooltip',
+        link: '/pages/modal-overlays/tooltip',
+      },
+    ],
+  },
+  {
+    title: 'Extra Components',
+    icon: 'message-circle-outline',
+    children: [
+      {
+        title: 'Calendar',
+        link: '/pages/extra-components/calendar',
+      },
+      {
+        title: 'Progress Bar',
+        link: '/pages/extra-components/progress-bar',
+      },
+      {
+        title: 'Spinner',
+        link: '/pages/extra-components/spinner',
+      },
+      {
+        title: 'Alert',
+        link: '/pages/extra-components/alert',
+      },
+      {
+        title: 'Calendar Kit',
+        link: '/pages/extra-components/calendar-kit',
+      },
+      {
+        title: 'Chat',
+        link: '/pages/extra-components/chat',
+      },
+    ],
+  },
+  {
+    title: 'Maps',
+    icon: 'map-outline',
+    children: [
+      {
+        title: 'Google Maps',
+        link: '/pages/maps/gmaps',
+      },
+      {
+        title: 'Leaflet Maps',
+        link: '/pages/maps/leaflet',
+      },
+      {
+        title: 'Bubble Maps',
+        link: '/pages/maps/bubble',
+      },
+      {
+        title: 'Search Maps',
+        link: '/pages/maps/searchmap',
+      },
+    ],
+  },
+  {
+    title: 'Charts',
+    icon: 'pie-chart-outline',
+    children: [
+      {
+        title: 'Echarts',
+        link: '/pages/charts/echarts',
+      },
+      {
+        title: 'Charts.js',
+        link: '/pages/charts/chartjs',
+      },
+      {
+        title: 'D3',
+        link: '/pages/charts/d3',
+      },
+    ],
+  },
+  {
+    title: 'Editors',
+    icon: 'text-outline',
+    children: [
+      {
+        title: 'TinyMCE',
+        link: '/pages/editors/tinymce',
+      },
+      {
+        title: 'CKEditor',
+        link: '/pages/editors/ckeditor',
+      },
+    ],
+  },
+  {
+    title: 'Tables & Data',
+    icon: 'grid-outline',
+    children: [
+      {
+        title: 'Smart Table',
+        link: '/pages/tables/smart-table',
+      },
+      {
+        title: 'Tree Grid',
+        link: '/pages/tables/tree-grid',
+      },
+    ],
+  },
+  {
+    title: 'Miscellaneous',
+    icon: 'shuffle-2-outline',
+    children: [
+      {
+        title: '404',
+        link: '/pages/miscellaneous/404',
+      },
+    ],
+  },
+  {
+    title: 'Auth',
+    icon: 'lock-outline',
+    children: [
+      {
+        title: 'Login',
+        link: '/auth/login',
+      },
+      {
+        title: 'Register',
+        link: '/auth/register',
+      },
+      {
+        title: 'Request Password',
+        link: '/auth/request-password',
+      },
+      {
+        title: 'Reset Password',
+        link: '/auth/reset-password',
+      },
+    ],
+  },
+];
